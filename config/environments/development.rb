9141f471e88b526d6a7202f6d1fe88d67127e55c
JenJen <jennifer7793@gmail.com>
2022-04-09 09:01:13 +0800

    allow request origins to setup new WebSocket connections

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_cable.allowed_request_origins = [
+    # Local address we use for our standalone client
+    'file://',
+  ]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
